86ea8bb37a771299303bf85e238be3f7ee3e1fb3
Tuan Shou Cheng <shoujhengduan@gmail.com>
2018-02-01 10:48:10 +0800

    Delete class
    Delete files

---
+++ Real Time Video Process/Class/View Controller/THPlayerViewController.h
@@ -0,0 +1,14 @@
+//
+//  THPlayerViewController.h
+//  Real Time Video Process
+//
+//  Created by Tuan Shou Cheng on 2018/1/19.
+//  Copyright © 2018年 Tuan Shou Cheng. All rights reserved.
+//
+
+@import AVKit;
+
+@interface THPlayerViewController : AVPlayerViewController
+@property (nonatomic, strong) AVURLAsset *asset;
+
+@end
